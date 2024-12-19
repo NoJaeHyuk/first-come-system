e711b6f23ccf829cead826935d550d20bbec20c9
NOJaeHyuk <jaehyuk7722@naver.com>
2024-12-19 12:15:03 +0900

    first commit

---
+++ src/main/java/com/firstcomesystem/FirstComeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.firstcomesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstComeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstComeSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/firstcomesystem/FirstComeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstcomesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstComeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
