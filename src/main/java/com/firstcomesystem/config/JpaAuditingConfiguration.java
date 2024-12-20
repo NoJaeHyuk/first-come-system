d1a07580064f57d8226e8664b6d35d601e5aaa1d
NOJaeHyuk <jaehyuk7722@naver.com>
2024-12-20 22:52:46 +0900

    feat: JpaAuditing 설정 및 AbstractEntity 생성

---
+++ src/main/java/com/firstcomesystem/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.firstcomesystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfiguration {
+}
