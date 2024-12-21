0ff3aac085e4b1fc31e231e811996d3454231812
NOJaeHyuk <jaehyuk7722@naver.com>
2024-12-21 15:14:56 +0900

    feat: 에러코드 추가

--- src/main/java/com/firstcomesystem/common/response/ErrorCode.java
+++ src/main/java/com/firstcomesystem/common/response/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
     COMMON_INVALID_PARAMETER("요청한 값이 올바르지 않습니다."),
     COMMON_ENTITY_NOT_FOUND("존재하지 않는 엔티티입니다."),
     COMMON_ILLEGAL_STATUS("잘못된 상태값입니다."),
+    COMMON_DUPLICATE("이미 존재하는 데이터입니다."),
+    EMAIL_SENDING("정상적으로 메일이 보내지지 않았습니다."),
 
     // 보안 관련 에러코드
     SECURITY_ERROR("Security operation failed."),
